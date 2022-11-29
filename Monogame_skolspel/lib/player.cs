d02e4857763e2df6dc97c392b9c33e383231770d
Alen Hajic <alha2011@edu.laroverken.se>
2022-11-29 10:59:32 +0100

    fixat animationen med gubben när han går runt

--- Monogame_skolspel/lib/player.cs
+++ Monogame_skolspel/lib/player.cs
@@ -15,6 +15,8 @@ namespace Monogame_skolspel.lib
 
         Dictionary<string, Rectangle> SpritePos = new Dictionary<string, Rectangle>();
 
+        int step = 0;
+        int delay = 0;
         
         public player(Texture2D _graphics)
         {
@@ -22,27 +24,27 @@ namespace Monogame_skolspel.lib
             position = new Vector2(200, 300);
             position2 = new Vector2(300, 300);
 
-            SpritePos.Add("normal", new Rectangle(10, 11, 60, 50));
-            SpritePos.Add("normal2", new Rectangle(70, 11, 60, 50));
-            SpritePos.Add("normal3", new Rectangle(120, 11, 60, 50));
-            SpritePos.Add("normal4", new Rectangle(180, 11, 60, 50));
+            SpritePos.Add("normal0", new Rectangle(10, 11, 60, 50));
+            SpritePos.Add("normal1", new Rectangle(75, 11, 60, 50));
+            SpritePos.Add("normal2", new Rectangle(140, 11, 60, 50));
+            SpritePos.Add("normal3", new Rectangle(200, 11, 60, 50));
 
-            SpritePos.Add("normalUp", new Rectangle(10, 200, 60, 305));
-            SpritePos.Add("normalUp2", new Rectangle(70, 200, 60, 305));
-            SpritePos.Add("normalUp3", new Rectangle(120, 200, 60, 305));
-            SpritePos.Add("normalUp4", new Rectangle(180, 200, 60, 305));
+            SpritePos.Add("normalUp0", new Rectangle(10, 200, 60, 305));
+            SpritePos.Add("normalUp1", new Rectangle(75, 200, 60, 305));
+            SpritePos.Add("normalUp2", new Rectangle(140, 200, 60, 305));
+            SpritePos.Add("normalUp3", new Rectangle(200, 200, 60, 305));
                 
                 
-            SpritePos.Add("normalLeft", new Rectangle(10, 77, 60, 50));
-            SpritePos.Add("normalLeft2", new Rectangle(70, 77, 60, 50));
-            SpritePos.Add("normalLeft3", new Rectangle(120, 77, 60, 50));
-            SpritePos.Add("normalLeft4", new Rectangle(180, 77, 60, 50));
+            SpritePos.Add("normalLeft0", new Rectangle(10, 77, 60, 50));
+            SpritePos.Add("normalLeft1", new Rectangle(75, 77, 60, 50));
+            SpritePos.Add("normalLeft2", new Rectangle(140, 77, 60, 50));
+            SpritePos.Add("normalLeft3", new Rectangle(200, 77, 60, 50));
 
 
-            SpritePos.Add("normalRight", new Rectangle(10, 140, 60, 50));
-            SpritePos.Add("normalRight2", new Rectangle(70, 140, 60, 50));
-            SpritePos.Add("normalRight3", new Rectangle(120, 140, 60, 50));
-            SpritePos.Add("normalRight4", new Rectangle(180, 140, 63, 50));
+            SpritePos.Add("normalRight0", new Rectangle(10, 140, 60, 50));
+            SpritePos.Add("normalRight1", new Rectangle(75, 140, 60, 50));
+            SpritePos.Add("normalRight2", new Rectangle(140, 140, 60, 50));
+            SpritePos.Add("normalRight3", new Rectangle(200, 140, 63, 50));
         }
 
         
@@ -52,27 +54,64 @@ namespace Monogame_skolspel.lib
             //Movement
             var ks = Keyboard.GetState();
 
+            if(step == 3)
+            {
+                step = 0;
+            }
+
+            if (delay < 0)
+            {
+                delay = 7;
+            }
+
             if (ks.IsKeyDown(Keys.A))
             {
                 position.X -= _speed;
+
+                if (delay == 0)
+                {
+                   
+                    step += 1;
+                }
+                delay--;
 
             }           
             
             if (ks.IsKeyDown(Keys.D))
             {
                 position.X += _speed;
-                
-                 
+
+                if (delay == 0)
+                {
+
+                    step += 1;
+                }
+                delay--;
+
             }
 
             if (ks.IsKeyDown(Keys.W))
             {
                 position.Y -= _speed;
+
+                if (delay == 0)
+                {
+
+                    step += 1;
+                }
+                delay--;
             }
 
             if (ks.IsKeyDown(Keys.S))
             {
                 position.Y += _speed;
+
+                if (delay == 0)
+                {
+
+                    step += 1;
+                }
+                delay--;
             }
 
 
@@ -88,25 +127,25 @@ namespace Monogame_skolspel.lib
 
             if (ks.IsKeyDown(Keys.W))
             {
-                spritebatch.Draw(texture, position, SpritePos["normalUp"], Color.White);
+                spritebatch.Draw(texture, position, SpritePos["normalUp" + step], Color.White);
             }
             else if (ks.IsKeyDown(Keys.A))
             {
-                spritebatch.Draw(texture, position, SpritePos["normalLeft"], Color.White);
+                spritebatch.Draw(texture, position, SpritePos["normalLeft" + step], Color.White);
             }
             else if (ks.IsKeyDown(Keys.D))
             {
-                spritebatch.Draw(texture, position, SpritePos["normalRight"], Color.White);
+                spritebatch.Draw(texture, position, SpritePos["normalRight" + step], Color.White);
             }
             else
             {
-                spritebatch.Draw(texture, position, SpritePos["normal"], Color.White);
+                spritebatch.Draw(texture, position, SpritePos["normal" + step], Color.White);
             }
 
 
 
-            //spritebatch.Draw(texture, position, SpritePos["normalRight"], Color.White);
-            //spritebatch.Draw(texture, position2, SpritePos["normalRight4"], Color.White);
+            //spritebatch.Draw(texture, position, SpritePos["normal0"], Color.White);
+            //spritebatch.Draw(texture, position2, SpritePos["normal1"], Color.White);
 
             //spritebatch.End();
         }
