590fa153a18bbd56cfb2c998bcd2811434352e5e
Alen Hajic <alha2011@edu.laroverken.se>
2023-05-05 17:51:42 +0200

    highscore, itemshop UI, fixa knapparna för att köpa olika saker bara

--- Monogame_skolspel/Game1.cs
+++ Monogame_skolspel/Game1.cs
@@ -14,6 +14,7 @@ using DocumentFormat.OpenXml.Drawing.Diagrams;
 using System.Text.Json.Nodes;
 using System.IO;
 using Newtonsoft.Json;
+using System.Linq;
 
 namespace Monogame_skolspel
 {
@@ -24,19 +25,17 @@ namespace Monogame_skolspel
         InGame,
         PauseGame
     }
-   
+
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private GameState ActiveState = GameState.MainMenu;
 
-        private string playerdata = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "highscoreS.json");
-
-
         private Texture2D background_1;
         private Texture2D background_2;
 
+        //enemy & player
         sprite_bullet player;
         private Texture2D _player;
         //sprite enemy;
@@ -44,31 +43,37 @@ namespace Monogame_skolspel
 
         sprite_bullet enemy;
 
+        //score
         private Texture2D scoreBG;
         private SpriteFont scoreOne;
         private SpriteFont scoreTwo;
         private SpriteFont scoreThree;
-        
+
+        //bullets
         sprite_bullet bullet;
         private Texture2D _bullet;
 
         sprite_bullet bulletTest;
         private Texture2D _bulletTest;
 
+        //random stuff
         private Rectangle Start;
         private Rectangle Start2;
         private Texture2D Start_color;
         private Vector2 position = new Vector2(10, 20);
 
+        //count
         private int count = 0;
         private SpriteFont counter;
 
+        //health
         private Rectangle life_red;
         private Rectangle life_green;
         private Texture2D Healht_green;
         int Health = 1310;
         double Health_pos_x = -3;
 
+        //buttons
         private Texture2D buttText;
         private Texture2D buttTextHov;
         private Texture2D currentButt;
@@ -80,6 +85,22 @@ namespace Monogame_skolspel
         private Texture2D currentTextScore;
         private Texture2D UI_bar;
 
+        //itemshop
+        private Texture2D itemshop_heart;
+        private Texture2D itemshop_bow;
+        private Texture2D itemshop_bullet_speed;
+
+        private exitBtn item_buy_heart;
+        private Rectangle item_heart_rect;
+
+        private Texture2D buy_main;
+        private Texture2D buy_hover;
+        private Texture2D buy_current;
+
+        private exitBtn item_buy_bow;
+        private exitBtn item_buy_bullet;
+
+        //backpack
         private backpack backpack;
         private Texture2D backText;
         private Texture2D backText_hover;
@@ -94,7 +115,7 @@ namespace Monogame_skolspel
         private Texture2D exit_hover;
         private Texture2D exitcurrent_text;
         private Rectangle exitRect;
-        
+
 
 
         private Rectangle mouseRect;
@@ -106,7 +127,7 @@ namespace Monogame_skolspel
         player s;
         enemy e;
         backpack g;
-        
+
 
         int bullet_delay = 1000;
         int bullet_time;
@@ -130,7 +151,12 @@ namespace Monogame_skolspel
         List<sprite_bullet> _sprites { get; set; } = new List<sprite_bullet>();
         List<sprite_bullet> _enemyList { get; set; } = new List<sprite_bullet>();
         List<sprite_bullet> _bulletList { get; set; } = new List<sprite_bullet>();
+
+        private string playerdata = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "highscoreS.json");
         private List<highscore> highscores { get; set; } = new List<highscore>();
+
+        private bool GotHighScore { get; set; } = false;
+
 
         public Game1()
         {
@@ -141,7 +167,8 @@ namespace Monogame_skolspel
 
             Health = 1310;
             count = 0;
-            
+            highscores = LoadHighScore();
+
         }
 
         protected override void Initialize()
@@ -204,6 +231,14 @@ namespace Monogame_skolspel
             exit_hover = Content.Load<Texture2D>("exit-hover");
             exitRect = new Rectangle(1050, 20, 100, 96);
 
+            //itemshop
+            buy_main = Content.Load<Texture2D>("buy-btn-main");
+            buy_hover = Content.Load<Texture2D>("buy-btn-hover");
+
+            itemshop_heart = Content.Load<Texture2D>("itemshop-heart-v2");
+            item_heart_rect = new Rectangle(0, 0, buy_main.Width, buy_main.Height);
+            itemshop_bow = Content.Load<Texture2D>("itemshop-bow-v2");
+            itemshop_bullet_speed = Content.Load<Texture2D>("itemshop-bullet-speed-v2");
             MouseState mouseState = Mouse.GetState();
             mouseRect = new Rectangle(mouseState.X, mouseState.Y, 100, 100);
 
@@ -215,7 +250,7 @@ namespace Monogame_skolspel
             //e = new enemy(this);
             //_enemyList.Add(e);
 
-           
+
 
         }
 
@@ -226,6 +261,9 @@ namespace Monogame_skolspel
 
             if (ActiveState == GameState.MainMenu)
             {
+                Health = 1310;
+                Health_pos_x = -3;
+                count = 0;
                 // kod som skapar en knapp
                 button = new Button(currentButt, buttRect, buttFont, "Start Game");
 
@@ -274,7 +312,7 @@ namespace Monogame_skolspel
                     currentTextScore = buttText;
                 }
 
-                if(scoreRect.Contains(mouseState.Position) && mouseState.LeftButton == ButtonState.Pressed)
+                if (scoreRect.Contains(mouseState.Position) && mouseState.LeftButton == ButtonState.Pressed)
                 {
                     ActiveState = GameState.Highscore;
                 }
@@ -285,11 +323,12 @@ namespace Monogame_skolspel
             //if (ActiveState == GameState.InGame) 
             //{ 
 
-            if(ActiveState == GameState.Highscore)
+            if (ActiveState == GameState.Highscore)
             {
                 backpack = new backpack(backcurrentText, backRect);
 
-                
+
+
 
 
                 exitBtn = new exitBtn(exitcurrent_text, exitRect);
@@ -307,7 +346,7 @@ namespace Monogame_skolspel
                     exitcurrent_text = exit_main;
                 }
 
-                if(exitRect.Contains(mouseState.Position) && mouseState.LeftButton == ButtonState.Pressed)
+                if (exitRect.Contains(mouseState.Position) && mouseState.LeftButton == ButtonState.Pressed)
                 {
                     ActiveState = GameState.MainMenu;
                 }
@@ -326,14 +365,14 @@ namespace Monogame_skolspel
                 if (backRect.Contains(mouseState.Position))
                 {
                     backcurrentText = backText_hover;
-                    
+
                 }
                 else
                 {
                     backcurrentText = backText;
                 }
-                
-                if(backRect.Contains(mouseState.Position) && mouseState.LeftButton == ButtonState.Pressed)
+
+                if (backRect.Contains(mouseState.Position) && mouseState.LeftButton == ButtonState.Pressed)
                 {
                     backpackUI = true;
                     ActiveState = GameState.PauseGame;
@@ -344,14 +383,14 @@ namespace Monogame_skolspel
                 if (exitRect.Contains(mouseState.Position))
                 {
                     exitcurrent_text = exit_hover;
-                    
+
                 }
                 else
                 {
                     exitcurrent_text = exit_main;
                 }
-    
-                if(exitRect.Contains(mouseState.Position) && mouseState.LeftButton == ButtonState.Pressed)
+
+                if (exitRect.Contains(mouseState.Position) && mouseState.LeftButton == ButtonState.Pressed)
                 {
                     backpackUI = false;
                 }
@@ -381,7 +420,7 @@ namespace Monogame_skolspel
 
 
                 step += gameTime.ElapsedGameTime.Milliseconds;
-                if(step > 4000)
+                if (step > 4000)
                 {
                     _enemyList.Add(new enemy(this));
                     step = 0;
@@ -408,7 +447,7 @@ namespace Monogame_skolspel
                     {
                         e.position_b.Y -= 2;
                     }
-                    
+
 
 
                     if (e.step == 3)
@@ -434,7 +473,7 @@ namespace Monogame_skolspel
                         Health_pos_x += 1;
                     }
 
-                   
+
 
                     foreach (bullet b in _bulletList)
                     {
@@ -453,10 +492,10 @@ namespace Monogame_skolspel
                             //}
 
 
-                           
-                           
+
+
                             b.IsActive = false;
-                            
+
 
                         }
 
@@ -488,14 +527,14 @@ namespace Monogame_skolspel
                     SaveScore(newHs);
 
                 }
-             
+
                 if (BombTime == 0)
                 {
                     int x = rnd.Next(0, GraphicsDevice.Viewport.Width - 60);
                     //Rando = rnd.Next(1, 2);
 
                     BombTime = Bomb_delay;
-                 
+
 
 
                     if (/*Rando == 1 &&*/ BombTime == 0)
@@ -576,7 +615,7 @@ namespace Monogame_skolspel
 
                 }
 
-             
+
 
                 //skapar en loop/timer för skoten så man kan ha delay 
                 bullet_time -= gameTime.ElapsedGameTime.Milliseconds;
@@ -626,7 +665,7 @@ namespace Monogame_skolspel
                     _bulletList.Add(new bullet(this)
                     {
 
-                        position_b = new Vector2(s.position_b.X  - 20 /*- 240*/, s.position_b.Y + 20 /*- 245*/)
+                        position_b = new Vector2(s.position_b.X - 20 /*- 240*/, s.position_b.Y + 20 /*- 245*/)
 
                     });
                 }
@@ -651,7 +690,7 @@ namespace Monogame_skolspel
                     });
                 }
 
-              
+
 
                 //foreach (enemy e in _enemyList)
                 //{
@@ -685,7 +724,7 @@ namespace Monogame_skolspel
 
                 base.Update(gameTime);
 
-                
+
 
 
             }
@@ -697,6 +736,10 @@ namespace Monogame_skolspel
                 backpack.Update();
 
                 exitBtn = new exitBtn(exitcurrent_text, exitRect);
+                item_buy_heart = new exitBtn(buy_main, item_heart_rect);
+
+
+
                 exitBtn.Update();
 
                 MouseState mouseState = Mouse.GetState();
@@ -704,6 +747,7 @@ namespace Monogame_skolspel
 
                 //backpackUI = true;
 
+                //exitBtn
                 if (exitRect.Contains(mouseState.Position))
                 {
                     exitcurrent_text = exit_hover;
@@ -718,75 +762,111 @@ namespace Monogame_skolspel
                     //backpackUI = false;
                     ActiveState = GameState.InGame;
                 }
+
+                //itemshopBtn
+                if (item_heart_rect.Contains(mouseState.Position))
+                {
+                    buy_main = buy_hover;
+                }
+                else
+                {
+                    buy_main = buy_main;
+                }
             }
         }
 
 
         private void SaveScore(highscore newScore)
         {
-            highscores.Add(newScore);
-
-            using (FileStream fs = File.Open(playerdata, FileMode.CreateNew))
-            using (StreamWriter sw = new StreamWriter(fs))
-            using (Newtonsoft.Json.JsonWriter jw = new JsonTextWriter(sw))
+            int minScore = 0;
+            int maxScore = 0;
+            if (highscores.Count > 0)
             {
-                jw.Formatting = Newtonsoft.Json.Formatting.Indented;
+                minScore = highscores.Min(x => x.Points);
+                maxScore = highscores.Max(x => x.Points);
+            }
+            if (newScore.Points > maxScore)
+            {
+                GotHighScore = true;
+            }
+            //om scoren är högre än lägsta i listan eller att det inte finns 5st.
+            if (newScore.Points > minScore | highscores.Count < 5)
+            {
+                highscores.Add(newScore);
+                highscores.Sort(delegate (highscore x, highscore y)
+                {
+                    return y.Points.CompareTo(x.Points);
+                });
+                //Plocka ut de fem högsta
+                if (highscores.Count > 5)
+                {
+                    highscores = highscores.Take(5).ToList();
+                }
 
-                Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
-                serializer.Serialize(jw, highscore);
 
+                if (File.Exists(playerdata))
+                {
+                    File.Delete(playerdata);
+                }
+
+                using (FileStream fs = File.Open(playerdata, FileMode.CreateNew))
+                using (StreamWriter sw = new StreamWriter(fs))
+                using (Newtonsoft.Json.JsonWriter jw = new JsonTextWriter(sw))
+                {
+                    jw.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+                    Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+                    serializer.Serialize(jw, highscores);
+                }
             }
         }
+
+        private List<highscore> LoadHighScore()
+        {
+            try
+            {
+                var content = File.ReadAllText(playerdata);
+                var lista = System.Text.Json.JsonSerializer.Deserialize<List<highscore>>(content);
+                return lista;
+            }
+            catch
+            {
+                return new List<highscore>();
+            }
+        }
+
 
         private void DrawHigScore()
         {
-         
-            if(ActiveState == GameState.Highscore)
+
+            //if (ActiveState == GameState.Highscore)
+            //{
+            if (highscores.Count > 0)
             {
+                float y = 100;
+                _spriteBatch.DrawString(counter, "High scores Top 5: ", new Vector2(250, 50), Color.White);
+
                 foreach (var hs in highscores)
                 {
-
-                    _spriteBatch.DrawString(scoreOne, hs.DateAdded + " " + hs.Name + " " + hs.Points, new Vector2(250, 100), Color.White);
+                    y += 70;
+                    _spriteBatch.DrawString(scoreOne, hs.DateAdded + " " + hs.Name + " " + hs.Points, new Vector2(250, y), Color.White);
 
                 }
             }
-           
+            //}
+
 
         }
+
+
+
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
 
-            if(ActiveState == GameState.InGame)
-            {
-                _spriteBatch.Draw(background_1, Vector2.Zero, Color.White);
-                _spriteBatch.Draw(background_2, Vector2.Zero, Color.White);
-                _sprites.ForEach(e => e.Draw(_spriteBatch));
-                _bulletList.ForEach(e => e.Draw(_spriteBatch));
-                _enemyList.ForEach(e => e.Draw(_spriteBatch));
-                backpack.Draw(_spriteBatch);
-                //_spriteBatch.Draw(Start_color, life_red, Color.Red);
-                _spriteBatch.Draw(UI_bar, new Vector2(0, 0), Color.White);
-                _spriteBatch.Draw(Healht_green, new Rectangle((int)Health_pos_x, -5, Health, 900), Color.White);
-                _spriteBatch.DrawString(counter, count.ToString(), new Vector2(155, 85), Color.WhiteSmoke);
-
-                if (backpackUI == true)
-                {
-                    _spriteBatch.Draw(backBG, new Vector2(0, - 50), Color.White);
-                    exitBtn.Draw(_spriteBatch);
-                    _spriteBatch.DrawString(scoreOne, count.ToString(), new Vector2(0, 0), Color.White);
-                }
-                
-            }
-            else if (ActiveState == GameState.MainMenu)
-            {
-                _spriteBatch.Draw(main_menu, Vector2.Zero, Color.White);
-                button.Draw(_spriteBatch);
-                highscore.Draw(_spriteBatch);
-            }
-            else if(ActiveState == GameState.PauseGame)
+            if (ActiveState == GameState.InGame)
             {
                 _spriteBatch.Draw(background_1, Vector2.Zero, Color.White);
                 _spriteBatch.Draw(background_2, Vector2.Zero, Color.White);
@@ -803,17 +883,48 @@ namespace Monogame_skolspel
                 {
                     _spriteBatch.Draw(backBG, new Vector2(0, -50), Color.White);
                     exitBtn.Draw(_spriteBatch);
+                    _spriteBatch.DrawString(scoreOne, count.ToString(), new Vector2(0, 0), Color.White);
+                }
+
+            }
+            else if (ActiveState == GameState.MainMenu)
+            {
+                _spriteBatch.Draw(main_menu, Vector2.Zero, Color.White);
+                button.Draw(_spriteBatch);
+                highscore.Draw(_spriteBatch);
+            }
+            else if (ActiveState == GameState.PauseGame)
+            {
+                _spriteBatch.Draw(background_1, Vector2.Zero, Color.White);
+                _spriteBatch.Draw(background_2, Vector2.Zero, Color.White);
+                _sprites.ForEach(e => e.Draw(_spriteBatch));
+                _bulletList.ForEach(e => e.Draw(_spriteBatch));
+                _enemyList.ForEach(e => e.Draw(_spriteBatch));
+                backpack.Draw(_spriteBatch);
+                //_spriteBatch.Draw(Start_color, life_red, Color.Red);
+                _spriteBatch.Draw(UI_bar, new Vector2(0, 0), Color.White);
+                _spriteBatch.Draw(Healht_green, new Rectangle((int)Health_pos_x, -5, Health, 900), Color.White);
+                _spriteBatch.DrawString(counter, count.ToString(), new Vector2(155, 85), Color.WhiteSmoke);
+
+                if (backpackUI == true)
+                {
+                    _spriteBatch.Draw(backBG, new Vector2(0, -50), Color.White);
+                    _spriteBatch.Draw(itemshop_heart, new Vector2(0, 50), Color.White);
+                    _spriteBatch.Draw(itemshop_bow, new Vector2(0, 50), Color.White);
+                    _spriteBatch.Draw(itemshop_bullet_speed, new Vector2(0, 50), Color.White);
+                    //_spriteBatch.Draw(buy_main, new Vector2(320, 300), Color.White);
+                    exitBtn.Draw(_spriteBatch);
+                    item_buy_heart.Draw(_spriteBatch);
                 }
             }
-            else if(ActiveState == GameState.Highscore)
+            else if (ActiveState == GameState.Highscore)
             {
                 _spriteBatch.Draw(scoreBG, Vector2.Zero, Color.White);
                 exitBtn.Draw(_spriteBatch);
                 DrawHigScore();
-                //_spriteBatch.DrawString(scoreOne, test.Points.ToString(), new Vector2(100, 100), Color.WhiteSmoke);
             }
 
-           
+
             _spriteBatch.End();
             // TODO: Add your drawing code here
 
